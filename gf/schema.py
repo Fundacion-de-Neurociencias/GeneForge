5b618a60f49a0285a6fc7708b0ccf871e35bfe2f
manuelmenendezgonzalez <manuelmenendezgonzalez@users.noreply.github.com>
2025-06-05 23:25:49 +0200

    🔄 Sync update - 2025-06-05 23:25:49

---
+++ gf/schema.py
@@ -0,0 +1,26 @@
+﻿# gf/schema.py
+
+NODE_SCHEMAS = {
+    "gene_edit": {
+        "required": ["type", "target_gene"],
+        "types": {
+            "type": str,
+            "target_gene": str
+        }
+    },
+    "construct": {
+        "required": ["name", "elements"],
+        "types": {
+            "name": str,
+            "elements": list
+        }
+    },
+    "logic_node": {
+        "required": [],
+        "types": {
+            "flag": bool,
+            "debug": bool,
+            "fallback": type(None)
+        }
+    }
+}
