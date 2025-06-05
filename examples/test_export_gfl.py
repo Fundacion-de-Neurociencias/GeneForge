8a3fe41db1ae6b7d008962c85f58e350a6d34bb1
manuelmenendezgonzalez <manuelmenendezgonzalez@users.noreply.github.com>
2025-06-05 20:15:49 +0200

    🧪 Add test_export_gfl.py example - 2025-06-05 20:15:49

---
+++ examples/test_export_gfl.py
@@ -0,0 +1,5 @@
+﻿from gf.genetics.nodes_genetics import GeneticTechnique
+from gf.export.gfl_exporter import export_to_gfl
+
+obj = GeneticTechnique(name="CRISPR", components=["Cas9", "sgRNA"], mechanism="DSB repair")
+print(export_to_gfl(obj))
