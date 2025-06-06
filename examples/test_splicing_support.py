ee0128ccc5be03f9213ab3ca350fb3950b0bd3dc
manuelmenendezgonzalez <manuelmenendezgonzalez@users.noreply.github.com>
2025-06-06 10:18:03 +0200

    🔄 Sync update - 2025-06-06 10:18:03

--- examples/test_splicing_support.py
+++ examples/test_splicing_support.py
@@ -1,6 +1,6 @@
 ﻿from gf.gfl_import.gfl_importer import load_gfl_string
 
-code = \"\"\" 
+code = """
 splicing_event {
     type = "alternative"
     exons = ["exon1", "exon2", "exon3"]
@@ -18,7 +18,7 @@ regulation {
     target = "MDM2"
     effect = "repression"
 }
-\"\"\"
+"""
 
 print("🧪 Testing advanced node structures (splicing, protein, regulation)...\n")
 
