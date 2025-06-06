aa4fa8e7ce99077177580a217fb71db6dcedd2f4
manuelmenendezgonzalez <manuelmenendezgonzalez@users.noreply.github.com>
2025-06-06 09:39:13 +0200

    🔄 Sync update - 2025-06-06 09:39:13

--- gf/gfl_import/gfl_importer.py
+++ gf/gfl_import/gfl_importer.py
@@ -1,9 +1,9 @@
-﻿from gf.parser import parse_gfl
+﻿from gf.parser import parse_tokens
 from gf.semantic import validate_ast
 
 def load_gfl_string(code):
     if isinstance(code, list):
         code = '\n'.join(code)
-    tokens = parse_gfl(code)
+    tokens = parse_tokens(code)
     ast = validate_ast(tokens)
     return ast
